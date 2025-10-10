8c8dcdff924849a680197998666a33bbfb5ae837
valeryden <valery.denisov@wiz.io>
2025-10-10 13:28:20 +0200

    Update backend.tf

--- azure/bootstrap/backend.tf
+++ azure/bootstrap/backend.tf
@@ -1,5 +1,8 @@
-
 terraform {
   backend "azurerm" {
+    resource_group_name  = "rg-bootstrap-6861"
+    storage_account_name = "sabootstrap6861"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
   }
 }
